5fe27559ec7260cf95d56d4ba508eb66daf2f797
leunggeorge <liangyzteam@163.com>
2016-09-13 10:48:00 +0800

    auto commit

--- bk.sh
+++ bk.sh
@@ -2,3 +2,12 @@
 cp _config.yml bk.sh ./LeungGeorge.github.io/
 cp -r ./scaffolds ./source ./LeungGeorge.github.io/
 
+
+location ~ ^/dashboard/tech-home/static {
+	            root /home/work/orp/template;
+	        }
+	location = /{
+	            if ( $http_host ~* "^(.*)msbutech\.baidu\.com$") {
+	            rewrite ^(.*) http://msbutech.baidu.com/dashboard/tech break;
+	            }
+	        }
