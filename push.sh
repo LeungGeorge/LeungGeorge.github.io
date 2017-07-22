f90cc0930a48aa9340cc1fa03395cb89ad1c111c
liangyuanzheng <liangyuanzheng@baidu.com>
2017-07-22 16:23:05 +0800

    auto commit

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cp baidu-verify-F90255460A.txt ./public/
 git pull
 
 hexo clean
 hexo g
 hexo d
 
+cp baidu-verify-F90255460A.txt ./public/
 git add --all
 git commit -m "auto commit"
 git push origin hexo
