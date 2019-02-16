d8f087237ac749028e5f82e6f6fce0536a32b95d
liangyuanzheng <liangyuanzheng@baidu.com>
2019-02-16 19:25:32 +0800

    auto commit

---
+++ hexo-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git pull
+
+hexo clean
+hexo g
+hexo d
+
+cp baidu-verify-F90255460A.txt ./public/
+
+hexo algolia
+
+git add --all
+git commit -m "auto commit"
+git push origin hexo
+
+git pull
+
