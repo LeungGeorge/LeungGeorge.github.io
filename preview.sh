e25e198b350cff531890d3440f274e27585e4d01
LeungGeorge <liangyzteam@163.com>
2023-08-16 19:11:44 +0800

    by 七点一刻

--- preview.sh
+++ preview.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp -r source/_posts/local.assets/ source/local.assets/
+cp -r source/_posts/assets/ source/assets/
 
 hexo clean
 # hexo generate
--- push.sh
+++ push.sh
@@ -2,6 +2,7 @@
 
 
 cp -r source/_posts/local.assets/ source/local.assets/
+cp -r source/_posts/assets/ source/assets/
 
 hexo clean
 hexo g
